a4b738b5b01ee266cebf2072f059130adacc07ee
Rajat Shenoy <rajat24499@gmail.com>
2020-07-29 16:47:33 +0530

    test js

--- test/test.js
+++ test/test.js
@@ -26,9 +26,9 @@ contract('AuctionContract', function (accounts) {
   //Test Case for checking if the bid is more than the token amount
   it("Should NOT allow to bid more than remaining tokens", function() {
     /**********
-    TASK 1:   Call bid method from accounts[1] of Auction.sol using auctionInstance and
+    Call bid method from accounts[1] of Auction.sol using auctionInstance and
     pass itemId=0, count=6 as arguments
-    HINT:     To make a function call from account 1 use {from: accounts[1]} as an extra argument
+    
     ***********/
     return auctionInstance.bid(0,6,{from:accounts[1]}).then(function (result) {
       /*
@@ -38,16 +38,8 @@ contract('AuctionContract', function (accounts) {
     }).catch(function (e) {
       var a = e.toString();
       if(e === "Failed to check remaining tokens less than count") {
-        /**********
-        TASK 2: This is the error which we had thrown. Should you assert true or false?
-        HINT:   Use assert(false) to assert false
-                Use assert(true) to assert true
-        ***********/
         assert(false);
       } else {
-        /**********
-        TASK 3: assert the opposite here
-        ***********/
         assert(true);
       }
     })
@@ -56,7 +48,7 @@ contract('AuctionContract', function (accounts) {
   //Modifier Checking
   it("Should NOT allow non owner to reveal winners", function() {
     /**********
-    TASK 4: Call revealWinners from account 1
+    Call revealWinners from account 1
     ***********/
      return auctionInstance.revealWinners({from:accounts[1]}).then(function (instance) {
        /*
@@ -65,16 +57,10 @@ contract('AuctionContract', function (accounts) {
        throw("Failed to check owner in reveal winners");
      }).catch(function (e) {
        if(e === "Failed to check owner in reveal winners") {
-         /**********
-         TASK 5: This is the error which we had thrown. Should you assert true or false?
-         HINT:   Use assert(false) to assert false
-                 Use assert(true) to assert true
-         ***********/
+
          assert(false);
        } else {
-         /**********
-         TASK 6: assert the opposite here
-         ***********/
+
          assert(true);
        }
      })
@@ -82,77 +68,52 @@ contract('AuctionContract', function (accounts) {
 
 
   it("Should set winners", function() {
-    /**********
-    TASK 7: Call register function from account 2
-    ***********/
+
     return auctionInstance.register({from:accounts[2]}).then(function(result) {
-      /**********
-      TASK 8: Call register function from account 3
-      ***********/
+
         return auctionInstance.register({from:accounts[3]});
     }).then(function() {
-      /**********
-      TASK 9: Call register function from account 4
-      ***********/
+
         return auctionInstance.register({from:accounts[4]});
     }).then(function() {
-      /**********
-      TASK 10: Call bid method from accounts[2] of Auction.sol using auctionInstance and
-      pass itemId=0, count=5 as arguments
-      ***********/
+
         return auctionInstance.bid(0,5,{from:accounts[2]});
     }).then(function() {
-      /**********
-      TASK 11: Call bid method from accounts[3] of Auction.sol using auctionInstance and
-      pass itemId=1, count=5 as arguments
-      ***********/
+
         return auctionInstance.bid(1,5,{from:accounts[3]});
     }).then(function() {
-      /**********
-      TASK 12: Call bid method from accounts[4] of Auction.sol using auctionInstance and
-      pass itemId=2, count=5 as arguments
-      ***********/
+
         return auctionInstance.bid(2,5,{from:accounts[4]});
     }).then(function() {
-      /**********
-      TASK 13: Call revealWinners function from accounts[0]
-      ***********/
+
         return auctionInstance.revealWinners({from:accounts[0]});
     }).then(function() {
-      /**********
-      TASK 14: call winners function from accounts[0] to get the winner of item id 0
-      ***********/
+
         return auctionInstance.winners(0);
     }).then(function(result) {
       /**********
-      TASK 15:  assert to see if the winner address is not the default address
-      HINT:     Default address is '0x0000000000000000000000000000000000000000'
-                Use notEqual method of assert
-                Parameters for notEqual : (result, default address , message);
+      assert to see if the winner address is not the default address
+      Default address is '0x0000000000000000000000000000000000000000'
       ***********/
       assert.notEqual(result,"0x0000000000000000000000000000000000000000","task15");
       /**********
-      TASK 16: call winners function from accounts[0] to get the winner of item id 1
+      call winners function from accounts[0] to get the winner of item id 1
       ***********/
       return auctionInstance.winners(1);
     }).then(function(result) {
       /**********
-      TASK 17:  assert to see if the winner address is not the default address
-      HINT:     Default address is '0x0000000000000000000000000000000000000000'
-                Use notEqual method of assert
-                Parameters for notEqual : (result, default address , message);
+      assert to see if the winner address is not the default address
+      Default address is '0x0000000000000000000000000000000000000000'
       ***********/
       assert.notEqual(result,"0x0000000000000000000000000000000000000000","task17");
       /**********
-      TASK 18: Call register function from account 3 to get the winner of item id 2
+      Call register function from account 3 to get the winner of item id 2
       ***********/
       return auctionInstance.winners(2);
     }).then(function(result) {
       /**********
-      TASK 19:  assert to see if the winner address is not the default address
-      HINT:     Default address is '0x0000000000000000000000000000000000000000'
-                Use notEqual method of assert
-                Parameters for notEqual : (result, default address , message);
+      assert to see if the winner address is not the default address
+      Default address is '0x0000000000000000000000000000000000000000'
       ***********/
       assert.notEqual(result,"0x0000000000000000000000000000000000000000","task19");
     })
